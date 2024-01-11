c13f058dfabfcd49248304eaf4b34b277db21eee
sherin john <sherinjohn41@gmail.com>
2024-01-11 17:53:48 +0530

    chessboard

---
+++ script.js
@@ -0,0 +1,12 @@
+const chessboard = document.getElementById('chessboard');
+for(let i = 0; i < 8; i++) {
+    for(let j = 0; j < 8; j++) {
+        let chessSquare = document.createElement('div');
+        chessSquare.className = 'chesssquare';
+        if((i + j) % 2 == 0){
+            chessSquare.style.backgroundColor = '#000';
+        }
+        chessboard.appendChild(chessSquare);
+
+    }
+}
